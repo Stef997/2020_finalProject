04ed7da959e29b6e7a6a6faa2f159f7462ae51f4
sheldoncross <sheldoncrosss@gmail.com>
2020-03-23 14:16:43 -0400

    ...

--- src/sample/FightBackground.java
+++ src/sample/FightBackground.java
@@ -6,7 +6,7 @@ import javafx.scene.layout.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-public class gFightBackground {
+public class FightBackground {
     Image backgroundURL = new Image(new FileInputStream("background/Background_Attack-1.png.png"));
 
     BackgroundImage backgroundimage = new BackgroundImage(backgroundURL,
